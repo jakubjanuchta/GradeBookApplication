cc3a1af8a8c1c25579b90439ace91474502cff17
Jakub <kuba.janu@interia.pl>
2023-04-02 15:07:54 +0200

    Add StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Name = name;
+            this.Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
